e29d3c424758e357fd3f049762c08e546ed5ff22
Abdallah <abdallahchaar90@gmail.comh>
2021-01-06 09:14:57 +0100

    träning

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*global console, alert*/
 
 /*
-*/
+
 
 var myName = "Abdallah ",
 
@@ -11,3 +11,19 @@ var myName = "Abdallah ",
 
     document.getElementById("test").innerHTML = 
     "My Name Is : " + myName + " , My Age Is: " + myAge + " And My Country Is " + myConuntry;
+
+    */
+
+   var myName = "Abdallah ",
+
+    myAge = 32,
+
+    myConuntry = "Syria";
+
+    document.getElementById("test").innerHTML = 
+
+    " My Name Is : " + "Abdallah" + "<br>" +
+
+    " My Age Is: " + 30 + "<br>" +
+
+    " My Country Is: " + "Syria";
