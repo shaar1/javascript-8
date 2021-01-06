3482260aa04c8edad95aad2f87bbd0bc32b809cc
Abdallah <abdallahchaar90@gmail.comh>
2021-01-06 09:09:58 +0100

    concatenetion

---
+++ main.js
@@ -0,0 +1,13 @@
+/*global console, alert*/
+
+/*
+*/
+
+var myName = "Abdallah ",
+
+    myAge = 32,
+
+    myConuntry = "Syria";
+
+    document.getElementById("test").innerHTML = 
+    "My Name Is : " + myName + " , My Age Is: " + myAge + " And My Country Is " + myConuntry;
